fe49b0a35834e5f624e8a5286a7ee61bc98ba75c
Al Rodriguez <ProgrammerAl@Outlook.com>
2023-08-06 15:29:58 -0400

    Initial commit of new C# project for the Pulumi provider

---
+++ src/ProgrammerAL.Pulumi.CloudflarePages.StaticFilesProvider/Provider/ClouadflarePagesStaticFilesProvider.cs
@@ -0,0 +1,19 @@
+﻿using Pulumi.Experimental.Provider;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerAL.Pulumi.CloudflarePages.StaticFilesProvider;
+
+public class ClouadflarePagesStaticFilesProvider : Provider
+{
+    private readonly IHost _host;
+
+    public ClouadflarePagesStaticFilesProvider(IHost host)
+    {
+        _host = host;
+    }
+}
---
+++ src/ProgrammerAL.Pulumi.CloudflarePages.StaticFilesProvider/Provider/Program.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammerAL.Pulumi.CloudflarePages.StaticFilesProvider;
+using Pulumi.Experimental.Provider;
+
+public static class Program
+{
+    public static Task Main(string[] args)
+    {
+        return Provider.Serve(args, "0.0.1", host => new ClouadflarePagesStaticFilesProvider(host), CancellationToken.None);
+    }
+}
